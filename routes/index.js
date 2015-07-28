2cf315e2c5a11fe1ac5cb6c05790cff516bae352
Francisco BC <franciscobc@yahoo.es>
2015-07-28 19:05:52 +0200

    Moderación comentario

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); 
+router.param('commentId', commentController.load); 
 
 // Definición de rutas de session
 router.get('/login', 						sessionController.new);
@@ -34,6 +35,8 @@ router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizCo
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', 	 commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+			sessionController.loginRequired, 	 commentController.publish);
 
 
 module.exports = router;
